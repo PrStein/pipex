c801da27f082c7ea916835aafa1c437657ca4c43
Elisée Sydney Adjiguidi <eliseesydneyadjiguidi@Pr-Stein.local>
2021-12-17 01:44:02 +0100

    done

--- include/pipex.h
+++ include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: sadjigui <sadjigui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 16:15:05 by sadjigui          #+#    #+#             */
-/*   Updated: 2021/12/14 16:15:07 by sadjigui         ###   ########.fr       */
+/*   Updated: 2021/12/17 01:41:41 by sadjigui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,18 @@
 
 # include <unistd.h>
 # include <stdio.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
 # include "../libft/libft.h"
 
+
 int    main(int ac, char **av, char **env);
-void   process_child();
+
+void	child(int *pipefd, char **av, char **env);
+void	parent(int *pipefd, char **av, char **env);
+
+char	*find_cmd(char *path_cmds, char **exe);
+char	*find_path_cmd(char **env);
+
 #endif
--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,18 +6,91 @@
 /*   By: sadjigui <sadjigui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 16:05:01 by sadjigui          #+#    #+#             */
-/*   Updated: 2021/12/14 16:15:10 by sadjigui         ###   ########.fr       */
+/*   Updated: 2021/12/17 01:41:22 by sadjigui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/pipex.h"
 
-void process_child(int *pipefd, int pipeid, char **av, char **env)
+char	*find_path_cmd(char **env)
 {
-    
+	int i;
+
+	i = 0;
+	while (!(ft_strncmp(env[i], "PATH=", 5) == 0))
+		i++;
+	return (env[i] + 5);
 }
 
-int main(int ac, char **av, char **env)
+char	*find_cmd(char *path_cmds, char **exe)
+{
+	char **all_path;
+	char *the_good_one;
+	char *full_path;
+	int i;
+
+	i = 0;
+	all_path = ft_split(path_cmds, ':');
+	while (all_path[i])
+	{
+		the_good_one = ft_strdup(all_path[i]);
+		full_path = ft_strjoin(the_good_one, exe[0]);
+		free(the_good_one);
+		if (access(full_path, F_OK) == 0)
+			return (full_path);
+		i++;
+	}
+	if (access(exe[0], F_OK) == 0)
+		return (exe[0]);
+	perror("ERROR ");
+	exit(1);
+}
+
+void	child(int *pipefd, char **av, char **env)
+{
+    int fd;
+	char *path_cmds;
+	char *cmd;
+	char **exe;
+
+	fd = open(av[1], O_RDONLY);
+	if (fd < 0)
+	{
+		perror("ERROR ");
+		exit(1);
+	}
+	path_cmds = find_path_cmd(env);
+	exe = ft_split(av[2], ' ');
+	cmd = find_cmd(path_cmds, exe);
+	dup2(pipefd[1], 1);
+	dup2(fd, 0);
+	close(pipefd[0]);
+	execve(cmd, exe, env);
+}
+
+void	parent(int *pipefd, char **av, char **env)
+{
+    int fd;
+	char *path_cmds;
+	char *cmd;
+	char **exe;
+
+	fd = open(av[4], O_WRONLY | O_CREAT | O_TRUNC, 0666);
+	if (fd < 0)
+	{
+		perror("ERROR ");
+		exit(1);
+	}
+	path_cmds = find_path_cmd(env);
+	exe = ft_split(av[3], ' ');
+	cmd = find_cmd(path_cmds, exe);
+	dup2(pipefd[0], 0);
+	dup2(fd, 1);
+	close(pipefd[1]);
+	execve(cmd, exe, env);
+}
+
+int	main(int ac, char **av, char **env)
 {
     if (ac == 5)
     {
@@ -29,13 +102,19 @@ int main(int ac, char **av, char **env)
             perror("ERROR ");
             exit(1);
         }
-        pipefd = fork();
-        if (pipefd == -1)
+        pipeid = fork();
+        if (pipeid == -1)
         {
-            perror("ERROR ")
+            perror("ERROR ");
             exit(1);
         }
-        if (pipefd == 0)
-            process_child(pipefd, pipeid, av, env);
+        if (pipeid == 0)
+            child(pipefd, av, env);
+		parent(pipefd, av, env);
     }
+	if (ac > 5)
+		ft_putstr_fd("Too many arguments\n", 2);
+	if (ac < 5)
+		ft_putstr_fd("Missing arguments\n", 2);
+	return (0);
 }
